e817231ba7add151ba5e23c1c3f9f61d0a9aa712
Kakarotg <1013978756@qq.com>
2022-02-05 10:55:22 +0800

    11.00

--- src/components/content/goods/GoodsListItem.vue
+++ src/components/content/goods/GoodsListItem.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="goods-item">
+  <div class="goods-item" @click="itemClick">
     <img :src="goodsItem.show.img" alt="">
     <div class="goods-info">
       <p>{{goodsItem.title}}</p>
@@ -19,6 +19,11 @@
           return []
         }
       }
+    },
+    methods:{
+      itemClick(){
+        this.$router.push('/detail')
+      }
     }
   }
 </script>
@@ -33,7 +38,7 @@
   }
 
   .goods-item img {
-    width: 100%;
+    width: 100%; 
     border-radius: 5px;
   }
 
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -4,59 +4,15 @@
     <home-swiper :banners="banners"/>
     <recommend-view :recommends="recommends"/>
     <feature-view/>
-    <tab-control class="tab-control" :titles="['流行','新款','精选']"/>
-    <goods-list :goods="goods['pop'].list"/>
+    <tab-control class="tab-control" 
+    :titles="['流行','新款','精选']"
+    @tabClick="tabClick"/>
+    <goods-list :goods="goods[currentType].list"/>
     <ul>
       <li>列表1</li>
       <li>列表2</li>
-      <li>列表3</li>
-      <li>列表4</li>
-      <li>列表5</li>
-      <li>列表6</li>
-      <li>列表7</li>
-      <li>列表8</li>
-      <li>列表9</li>
-      <li>列表10</li>
-      <li>列表11</li>
-      <li>列表12</li>
-      <li>列表13</li>
-      <li>列表14</li>
-      <li>列表15</li>
-      <li>列表16</li>
-      <li>列表17</li>
-      <li>列表18</li>
-      <li>列表19</li>
-      <li>列表20</li>
-      <li>列表21</li>
-      <li>列表22</li>
-      <li>列表23</li>
-      <li>列表24</li>
-      <li>列表25</li>
-      <li>列表26</li>
-      <li>列表27</li>
-      <li>列表28</li>
-      <li>列表29</li>
-      <li>列表30</li>
-      <li>列表31</li>
-      <li>列表32</li>
-      <li>列表33</li>
-      <li>列表34</li>
-      <li>列表35</li>
-      <li>列表36</li>
-      <li>列表37</li>
-      <li>列表38</li>
-      <li>列表39</li>
-      <li>列表40</li>
-      <li>列表41</li>
-      <li>列表42</li>
-      <li>列表43</li>
-      <li>列表44</li>
-      <li>列表45</li>
-      <li>列表46</li>
-      <li>列表47</li>
-      <li>列表48</li>
-      <li>列表49</li>
-      <li>列表50</li>
+      <li>列表2</li>
+      <li>列表2</li>
     </ul>
   </div>
 </template>
@@ -92,7 +48,8 @@
           'pop':{page:0,list:[]},
           'new':{page:0,list:[]},
           'sell':{page:0,list:[]}
-        }
+        },
+        currentType:'pop'
       }
     },
     created(){
@@ -104,6 +61,19 @@
       this.getHomeGoods('sell')
     },
     methods:{
+      tabClick(index){
+        switch (index) {
+          case 0:
+            this.currentType='pop'
+            break;
+          case 1:
+            this.currentType='new'
+            break;
+          case 2:
+            this.currentType='sell'
+            break;
+        }
+      },
       getHomeMultidata(){
         getHomeMultidata().then(res => {
           console.log(res)
