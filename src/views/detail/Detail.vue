81933db4002b3db692f63ff843fda74c4dd3e876
Kakarotg <1013978756@qq.com>
2022-02-05 15:01:14 +0800

    15.01

--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -1,11 +1,25 @@
 <template>
-  <div>详情页</div>
+  <div id="detail">
+    <detail-nav-bar/>
+  </div>
 </template>
 
 <script>
-export default {
-  name:'Detail'
-}
+  import DetailNavBar from './childComps/DetailNavBar'
+  export default {
+    name:'Detail',
+    components:{
+      DetailNavBar
+    },
+    data() {
+      return {
+        iid: null,
+      }
+    },
+    created() {
+      this.iid=this.$route.params.iid
+    },
+  }
 </script>
 
 <style scoped>
