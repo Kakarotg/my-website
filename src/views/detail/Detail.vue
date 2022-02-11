1f7487a7053fc135fbf3e9f1c1ae5c18958988aa
Kakarotg <1013978756@qq.com>
2022-02-11 16:14:09 +0800

    16.14

--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -1,20 +1,25 @@
 <template>
   <div id="detail">
     <detail-nav-bar/>
+    <detail-swiper :top-images="topImages"/>
   </div>
 </template>
 
 <script>
   import DetailNavBar from './childComps/DetailNavBar'
+  import DetailSwiper from './childComps/DetailSwiper'
+
   import {getDetail} from "@/network/detail"
   export default {
     name:'Detail',
     components:{
-      DetailNavBar
+      DetailNavBar,
+      DetailSwiper
     },
     data() {
       return {
         iid: null,
+        topImages:[]
       }
     },
     created() {
@@ -23,7 +28,7 @@
       
       //根据iid获取详情数据
       getDetail(this.iid).then(res=>{
-        console.log(res)
+        this.topImages=res.data.result.itemInfo.topImages
       })
     },
   }
